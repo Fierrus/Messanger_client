c98b583b9ce2bdc0bacb5e9b6446369e7a9f328d
Fierrus <fierruspirrion@gmail.com>
2017-03-23 22:26:07 +0300

    Comments added

--- Socket/socket_cl.py
+++ Socket/socket_cl.py
@@ -4,17 +4,17 @@ from time import sleep
 
 
 
-class socket_thread:
+class socket_thread: 	#Создание класса для использования как потока
 	def __call__(self):
 		while a:
 			print('I am running')
-			self.data = soc.recv(1024)
-			self.data = self.data.decode('utf-8')
+			self.data = soc.recv(1024)	# Прием сокетом 1024 байт
+			self.data = self.data.decode('utf-8')	#Декодирование из байт в utf-8
 			if self.data == 'PING':
 				soc.send(b'PONG')
 				sleep(60)
 			else:
-				message_r = self.data
+				message_r = self.data	#В случае если это полезное сообщение, оно присваивается переменной
 			print('Server said ', self.data)
 			sleep(1)
 
@@ -24,9 +24,9 @@ class socket_thread:
 
 if __name__ == '__main__':
 	a=1
-	soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	#Параметры семейств сокетов
 	server_adress = ('localhost',10080)
-	try:
+	try:	#Использование эксепшенов себе на пользу
 		soc.connect((server_adress))
 		sc_listen = socket_thread()
 		t = threading.Thread(target=sc_listen)
@@ -35,7 +35,7 @@ if __name__ == '__main__':
 		print('Server is down or address is incorrect')
 	sleep(10)
 	a=0
-	soc.send(b'STOP')
+	soc.send(b'STOP')	#Комманда серверу на закрытие сокета
 	soc.close()
 
 
